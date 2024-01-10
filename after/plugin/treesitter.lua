9cf824d3833333dd179101eba14caefd0dba54af
Jason Ng <116290832+jason490@users.noreply.github.com>
2024-01-10 17:46:24 -0500

    added templ and html

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "javascript", "c", "go", "lua", "cpp", "java", "python", "css" },
+  ensure_installed = { "javascript", "c", "go", "lua", "cpp", "java", "python", "css", "templ","html" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
