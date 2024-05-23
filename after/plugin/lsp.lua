86393a4323d0052863cc1dae01aa3f54cc0ae66f
Jason Ng <116290832+jason490@users.noreply.github.com>
2024-05-23 15:07:01 -0400

    add_encoding

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -43,3 +43,12 @@ cmp.setup({
         ['<C-d>'] = cmp.mapping.scroll_docs(4),
     })
 })
+
+local cmp_nvim_lsp = require "cmp_nvim_lsp"
+require("lspconfig").clangd.setup {
+    capabilities = cmp_nvim_lsp.default_capabilities(),
+    cmd = {
+        "clangd",
+        "--offset-encoding=utf-16",
+    },
+}
